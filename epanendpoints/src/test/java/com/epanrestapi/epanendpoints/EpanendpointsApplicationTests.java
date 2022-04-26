3a031600f33e8624db4eab6c967582bad12c5956
anushka12333 <71206113+anushka12333@users.noreply.github.com>
2022-04-26 14:59:45 +0530

    Add files via upload
    Backend

---
+++ epanendpoints/src/test/java/com/epanrestapi/epanendpoints/EpanendpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epanrestapi.epanendpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpanendpointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
